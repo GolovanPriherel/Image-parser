7d91be39a7c4f2e346b5db1e5e6c18e963fe52fa
Ilya Manakinson <ilyamanakinson@MacBook-Air-Ilya.local>
2022-10-24 22:52:14 +0300

    add modules

--- src/modules/parser.py
+++ src/modules/parser.py
@@ -1,3 +1,11 @@
 import selenium
 import lxml
 
+from src.settings import parser_settings
+
+class Parser:
+    def __init__(self):
+        self.main_url = parser_settings.website_url
+
+    def parsing(self):
+        pass
---
+++ src/settings/__init__.py
@@ -0,0 +1,3 @@
+from src.settings.parser_settings import ParserSettings
+
+parser_settings = ParserSettings()
